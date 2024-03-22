3339e047d7aa77a46f1b53358ecf6e16f1f0e8a3
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2024-03-22 08:25:30 +0530

    added fat jar with duplicate Strategy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,11 +10,17 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        String url = "https://example.com";
+        dealingwithOkHttp(url);
+    }
+
+    public static void dealingwithOkHttp(String url){
         //helps to do http Calls easily raw java is complicated
         OkHttpClient client = new OkHttpClient();
 
 //        String url = "https://jsonplaceholder.typicode.com/todos/1";
-          String url = "https://example.com";
+
 
         //builder pattern
         Request request = new Request.Builder()
@@ -22,12 +28,21 @@ public class Main {
                 .build();
 
         try (Response response = client.newCall(request).execute()){
-            if(!response.isSuccessful()){
-                System.out.println("Something went wrong !!!");
-            }
-            System.out.println(response.body().string());
+          if(response.isSuccessful() && response.body() != null){
+              System.out.println("------ OKHTTP-------");
+              System.out.println(response.body().string());
+              System.out.println("-----------------------");
+          } else {
+              System.out.println("Something went wrong");
+          }
         } catch (IOException ex){
             ex.printStackTrace();
         }
+    }
+
+
+
+    public static void dealingWithRetrofit(String url){
+
     }
 }
